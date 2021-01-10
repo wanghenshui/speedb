638f3ce56c148800d87d6886841aab3b834ce411
hilikyochai <hilik.yochai@gmail.com>
2021-01-10 06:54:22 +0000

    disable low priority compaction

--- db/compaction/compaction_picker_hybrid.cc
+++ db/compaction/compaction_picker_hybrid.cc
@@ -81,7 +81,8 @@ bool HybridCompactionPicker::NeedsCompaction(
     }
   }
   // reduce number of sorted run ....
-  if (runningDesc[0].nCompactions == 0 && compactions_in_progress()->empty() &&
+  if (0 && runningDesc[0].nCompactions == 0 &&
+      compactions_in_progress()->empty() &&
       vstorage->LevelFiles(0).size() + 2 < multiplier_[0]) {
     for (uint hyperLevelNum = 1; hyperLevelNum <= curNumOfHyperLevels_;
          hyperLevelNum++) {
@@ -235,7 +236,7 @@ Compaction* HybridCompactionPicker::PickCompaction(
   }
 
   // no compaction check for reduction
-  if (vstorage->LevelFiles(0).size() + 2 < multiplier_[0] &&
+  if (0 && vstorage->LevelFiles(0).size() + 2 < multiplier_[0] &&
       runningDesc[0].nCompactions == 0 && compactions_in_progress()->empty()) {
     uint maxH = 0;
     for (uint hyperLevelNum = 1; hyperLevelNum <= curNumOfHyperLevels_;
