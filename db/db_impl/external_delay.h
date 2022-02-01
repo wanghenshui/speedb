67b1901a2a1a871732cb232fc8694804b9dfcd4f
Isaac Garzon <isaac@speedb.io>
2022-02-01 12:06:16 +0200

    SPDB-496: external_delay: avoid signed integer overflow
    UBSAN reported a signed integer overflow on the subtraction inside the
    std::min() call due to the conversion to an int. Use proper std::chrono
    types to make the code more self explanatory regarding units used and have
    higher precision. We still don't expect to wait more than 35 minutes per
    write (which is the maximum amount of microseconds that a signed int is
    able to represent) since we're limiting the sleep time to a maximum of a
    second, so the conversion to in the call to clock->SleepForMicroseconds()
    is OK.

--- db/db_impl/external_delay.h
+++ db/db_impl/external_delay.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <algorithm>
 #include <atomic>
 #include <chrono>
 #include <memory>
@@ -23,57 +22,17 @@ class ExternalDelay {
  private:
   using Nanoseconds = std::chrono::nanoseconds;
 
-  static constexpr auto kNanosPerSec =
+  static constexpr Nanoseconds::rep kNanosPerSec =
       std::chrono::duration_cast<Nanoseconds>(std::chrono::seconds(1)).count();
 
-  static constexpr auto kSleepNanosMin =
-      std::chrono::duration_cast<Nanoseconds>(std::chrono::microseconds(100))
-          .count();
-  static constexpr auto kSleepNanosMax =
-      std::chrono::duration_cast<Nanoseconds>(std::chrono::seconds(1)).count();
+  static constexpr Nanoseconds kSleepMin =
+      std::chrono::duration_cast<Nanoseconds>(std::chrono::microseconds(100));
+  static constexpr Nanoseconds kSleepMax =
+      std::chrono::duration_cast<Nanoseconds>(std::chrono::seconds(1));
 
   std::shared_ptr<SystemClock> clock_;
   std::atomic<double> delay_per_byte_nanos_;
-  std::atomic<size_t> next_request_time_;
+  std::atomic<uint64_t> next_request_time_nanos_;
 };
 
-inline void ExternalDelay::Enforce(size_t byte_count) {
-  if (delay_per_byte_nanos_ > 0) {
-    const auto start_time = clock_->NowNanos();
-    const auto delay_mul = 1.0 + (1.0 / kNanosPerSec * byte_count);
-    const auto current_delay =
-        delay_per_byte_nanos_.load(std::memory_order_acquire);
-    // We just need the delay per byte to be written atomically, but we don't
-    // really care if another thread wins and sets the delay that it calculated.
-    delay_per_byte_nanos_.store(current_delay * delay_mul,
-                                std::memory_order_release);
-
-    const auto added_delay = size_t(byte_count * current_delay);
-    const auto request_time =
-        added_delay +
-        next_request_time_.fetch_add(added_delay, std::memory_order_relaxed);
-    const auto sleep_time =
-        std::min(int(request_time) - int(start_time), int(kSleepNanosMax));
-    if (sleep_time > 0 && sleep_time > kSleepNanosMin) {
-      const auto sleep_micros =
-          std::chrono::duration_cast<std::chrono::microseconds>(
-              Nanoseconds(sleep_time))
-              .count();
-      clock_->SleepForMicroseconds(int(sleep_micros));
-    }
-  }
-}
-
-inline size_t ExternalDelay::SetDelayWriteRate(size_t new_rate) {
-  double old_delay = 0;
-  if (new_rate == 0) {
-    old_delay = delay_per_byte_nanos_.exchange(0, std::memory_order_release);
-  } else {
-    next_request_time_.store(clock_->NowNanos(), std::memory_order_release);
-    old_delay = delay_per_byte_nanos_.exchange(1.0 * kNanosPerSec / new_rate,
-                                               std::memory_order_release);
-  }
-  return old_delay == 0 ? 0 : static_cast<size_t>(kNanosPerSec / old_delay);
-}
-
-};  // namespace ROCKSDB_NAMESPACE
+}  // namespace ROCKSDB_NAMESPACE
